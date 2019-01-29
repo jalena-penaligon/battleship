52b04d65a3e5c2bcb48f189b2fb0f895f6296dd7
Mills Provosty <millsprovosty@gmail.com>
2019-01-29 14:56:22 -0700

    Adding cell class and test

---
+++ test/cell_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+  def test_it_exists
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+
+  def test_does_it_have_a_ship
+    cell = Cell.new("B4")
+
+    assert_nil cell.ship
+  end
+
+  def test_is_cell_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  
+end
