ec6ce034e5bfee63d74ee0be269aeeac8b6cf1eb
Mills Provosty <millsprovosty@gmail.com>
2019-01-29 15:15:35 -0700

    Added place_ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,13 +1,21 @@
 class Cell
   attr_reader :coordinate
+  attr_accessor :ship
+
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
   end
 
   def ship
+    @ship
   end
 
   def empty?
-    ship == nil 
+    @ship == nil
+  end
+
+  def place_ship(ship_type)
+    @ship = ship_type
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -22,5 +22,12 @@ class CellTest < Minitest::Test
     assert_equal true, cell.empty?
   end
 
-  
+  def test_can_you_place_a_ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    assert_equal cruiser, cell.ship
+    assert_equal false, cell.empty?
+  end
 end
