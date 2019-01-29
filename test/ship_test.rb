cf831ecdddcb9aae20776a8a3842a9bfc9cd0b23
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-29 11:55:13 -0700

    Adds Ship_Test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,5 +4,48 @@ require './lib/ship'
 
 require 'pry'
 
-class ShipTest < Minitest::ShipTest
-end 
+class ShipTest < Minitest::Test
+
+def test_it_exists
+  cruiser = Ship.new("Cruiser", 3)
+  assert_instance_of Ship, cruiser
+end
+
+def test_it_has_a_name
+  cruiser = Ship.new("Cruiser", 3)
+  assert_equal "Cruiser", cruiser.name
+end
+
+def test_it_has_a_length
+  cruiser = Ship.new("Cruiser", 3)
+  assert_equal 3, cruiser.length
+end
+
+def test_you_can_count_health
+  cruiser = Ship.new("Cruiser", 3)
+  assert_equal 3, cruiser.health
+end
+
+def test_is_it_sunk
+  cruiser = Ship.new("Cruiser", 3)
+  assert_equal false, cruiser.sunk?
+end
+
+def test_it_takes_a_hit_but_not_sunk_yet
+  cruiser = Ship.new("Cruiser", 3)
+  cruiser.hit
+  assert_equal 2, cruiser.health
+  assert_equal false, cruiser.sunk?
+end
+
+def test_it_takes_hits_till_it_sinks
+  cruiser = Ship.new("Cruiser", 3)
+  cruiser.hit
+  cruiser.hit
+  assert_equal 1, cruiser.health
+  cruiser.hit
+  assert_equal 0, cruiser.health
+  assert_equal true, cruiser.sunk?
+end
+
+end
