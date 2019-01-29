b999087c7be218fef3feae701c75843fa7ab12e5
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-29 11:54:55 -0700

    Adds Ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,20 @@
 class Ship
-end 
+  attr_reader :name, :length
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health_count = length
+  end
+
+  def health
+    @health_count
+  end
+
+  def sunk?
+    @health_count == 0
+  end
+
+  def hit
+    @health_count -= 1
+  end
+end
