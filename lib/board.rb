f0a700df3648ba8c737515180db90c7bc2062498
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-30 16:59:46 -0700

    Working on consecutive coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -29,15 +29,46 @@ class Board
     ship.length == coordinates.count
   end
 
-  def coords_are_consecutive
+  def coords_are_consecutive(ship, coordinates)
+    binding.pry
+    #splits coordinates into 2 separate objects
     coordinates.map! do |coordinate|
       coordinate.split(//)
+    end
+
+    #creates empty arrays to store first and second coordinates
+    first_coord = []
+    second_coord = []
+
+    #creates empty array to store ordinal value of letters
+    ordinal = []
+
+    #shovels first and second coordinates into empty arrays & converts number strings to integers
+    coordinates.each do |coordinate_pair|
+      first_coord << coordinate_pair[0]
+      second_coord << coordinate_pair[1].to_i
+    end
+
+    #convert letters to ordinals and shovels into empty array
+    first_coord.each do |coordinate|
+      ordinal << coordinate.ord
+    end
+
+    if
+      if first_coord.uniq.count == 1 && second_coord.sort.each_cons(2).all? { |x,y| y == x + 1 } == true
+        return true
+      end
+    elsif
+      if second_coord.uniq.count == 1 && ordinal.sort.each_cons(2).all? { |x,y| y == x + 1 } == true
+        return true
+      end
+    else
+      return false
     end
   end
 
   def valid_placement?(ship, coordinates)
-    ship_length_placement(ship, coordinates)
+    # ship_length_placement(ship, coordinates) &&
+    coords_are_consecutive(ship, coordinates)
   end
-
-
 end
