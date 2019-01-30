b8e2ec31a470077416879035eb072f6b2964fee9
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-30 16:34:04 -0700

    Updated board_test for all valid placement tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,7 +7,7 @@ require './lib/board'
 class BoardTest < Minitest::Test
   def test_board_exists
     board = Board.new
-    
+
   assert_instance_of Board, board
   end
 
@@ -33,5 +33,37 @@ class BoardTest < Minitest::Test
     board = Board.new
 
     assert_equal 16, board.cells.values.count
+  end
+
+  def test_that_coordinates_are_true
+    board = Board.new
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+  end
+
+  def test_that_coordinates_are_false
+    board = Board.new
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
+
+  def test_placement_matches_length_of_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_coordinates_are_consecutive
+    skip
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
   end
 end
