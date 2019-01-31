957ee0aba9f5ed861a9331287a9d12ea62f48172
Mills Provosty <millsprovosty@gmail.com>
2019-01-31 13:18:12 -0700

    Splitting into seperate methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,11 @@
 class Board
-  attr_reader :cells
+  attr_reader :cells,
+              :letter_coord,
+              :number_coord,
+              :ordinal,
+              :test_1,
+              :test_2
+
   def initialize
     @cells = {
       "A1" => Cell.new("A1"),
@@ -19,6 +25,9 @@ class Board
       "D3" => Cell.new("D3"),
       "D4" => Cell.new("D4"),
     }
+    @letter_coord = []
+    @number_coord = []
+    @ordinal = []
   end
 
   def valid_coordinate?(coordinate)
@@ -29,33 +38,42 @@ class Board
     ship.length == coordinates.count
   end
 
-  def coords_are_consecutive(ship, coordinates)
+  def split_coordinates(ship, coordinates)
     #splits coordinates into 2 separate objects
     coordinates.map! do |coordinate|
       coordinate.split(//)
     end
+  end
 
+  def stores_split_coordinates(ship, coordinates)
     #creates empty arrays to store letter and number coordinates
     letter_coord = []
     number_coord = []
-
-    #creates empty array to store ordinal value of letters
-    ordinal = []
-
     #shovels letter and number coordinates into empty arrays & converts number strings to integers
     coordinates.each do |coordinate_pair|
-      letter_coord << coordinate_pair[0]
-      number_coord << coordinate_pair[1].to_i
+      @letter_coord << coordinate_pair[0]
+      @number_coord << coordinate_pair[1].to_i
     end
+  end
 
+  def convert_store_letters_to_ordinals(ship, coordinates)
+    #creates empty array to store ordinal value of letters
+    @ordinal = []
     #convert letters to ordinals and shovels into empty array
     letter_coord.each do |coordinate|
       ordinal << coordinate.ord
-    end
+      end
+  end
+
+  def are_coordinates_unique_and_consecutive(ship, coordinates)
 
     test_1 = letter_coord.uniq.count == 1 && number_coord.sort.each_cons(2).all? { |x,y| y == x + 1 } == true
+
     test_2 = number_coord.uniq.count == 1 && ordinal.sort.each_cons(2).all? { |x,y| y == x + 1 } == true
-    # binding.pry
+  end
+
+
+  def placement_return_values(ship, coordinates)
     if test_1 || test_2
       true
     else
@@ -64,6 +82,12 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    ship_length_placement(ship, coordinates) && coords_are_consecutive(ship, coordinates)
+    split_coordinates(ship, coordinates)
+    stores_split_coordinates(ship, coordinates)
+    convert_store_letters_to_ordinals(ship, coordinates)
+    are_coordinates_unique_and_consecutive(ship, coordinates)
+    ship_length_placement(ship, coordinates) &&
+    placement_return_values(ship, coordinates)
+
   end
 end
