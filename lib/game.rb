ea8b6b0d75275da474d5d9bc27349d4f349c449f
Mills Provosty <millsprovosty@gmail.com>
2019-02-03 12:46:02 -0700

    Created a Game class and new game

---
+++ lib/game.rb
@@ -0,0 +1,36 @@
+class Game
+  attr_reader :comp_board,
+              :user_board
+  def initialize(comp_board, user_board)
+    @comp_board = comp_board
+    @user_board = user_board
+  end
+
+  def comp_placement(ship_type)
+    keys = @comp_board.cells.keys
+    select = ship_type.length
+    random = []
+    key_1 = keys.sample
+    random << key_1
+    key_index = keys.index(key_1)
+    (select - 1).times do
+      random << keys[key_index += 1]
+    end
+    loop do
+      if @comp_board.valid_placement?(ship_type, random) == true
+          random.each do |cell|
+            @comp_board.cells[cell].place_ship(ship_type)
+          end
+        break
+      else @comp_board.valid_placement?(ship_type, random) == false
+        random = []
+        key_1 = keys.sample
+        random << key_1
+        key_index = keys.index(key_1)
+        (select - 1).times do
+          random << keys[key_index += 1]
+        end
+      end
+    end
+  end
+end
