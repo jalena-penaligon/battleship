d6e5162312879cd88b728532b429493324894120
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-04 15:14:50 -0700

    Split computer and player class out of game

--- lib/game.rb
+++ lib/game.rb
@@ -1,47 +1,9 @@
 class Game
-  attr_reader :comp_board,
-              :user_board
-  def initialize(comp_board, user_board)
-    @comp_board = comp_board
-    @user_board = user_board
+  attr_reader :computer,
+              :player
+  def initialize(computer, player)
+    @computer = computer
+    @player = player
   end
 
-  def comp_placement(ship_type)
-    keys = @comp_board.cells.keys
-    select = ship_type.length
-    random = []
-    key_1 = keys.sample
-    random.unshift(key_1)
-    key_index = keys.index(key_1)
-      (select - 1).times do
-      random.unshift(keys[key_index -= 1])
-      end
-    valid_placement = @comp_board.valid_placement?(ship_type, random)
-
-    until valid_placement == true
-
-      random = []
-      key_1 = keys.sample
-      random.unshift(key_1)
-      key_index = keys.index(key_1)
-      (select - 1).times do
-        random.unshift(keys[key_index -= 1])
-      end
-      valid_placement = @comp_board.valid_placement?(ship_type, random)
-    end
-    random.each do |cell|
-      @comp_board.cells[cell].place_ship(ship_type)
-    end
-  end
-
-  def user_placement(ship_type, user_input)
-    until @user_board.valid_placement?(ship_type, user_input) == true
-      puts "Those are invalid coordinates. Please try again."
-      user_input = gets.chomp
-    end
-
-    user_input.each do |coordinate|
-      @user_board.cells[coordinate].place_ship(ship_type)
-    end
-  end
 end
