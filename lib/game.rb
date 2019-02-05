f7c61518a08ec9f87d511368c4277e182db2a492
Mills Provosty <millsprovosty@gmail.com>
2019-02-04 17:20:15 -0700

    Added computer and player turns

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,19 @@ class Game
     @player = player
   end
 
+  def generate_random_coord
+    options = player.cells.keys
+    guess = options.shuffle.pop
+    options.delete(guess)
+    return guess
+  end
+
+  def computer_take_turn(coordinate)
+    coordinate = generate_random_coord
+    player.board.cells[coordinate].fire_upon
+  end
+
+  def player_take_turn(coordinate)
+    computer.board.cells[coordinate].fire_upon
+  end
 end
